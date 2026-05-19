c6750c32b46ef7e3a129646cfd9dc779244be32a
vivekpunde05 <vivekpunde6@gmail.com>
2026-05-19 21:27:41 +0530

    Updated project files

--- connection.py
+++ connection.py
@@ -34,6 +34,7 @@ def execute(query, params=None, fetch=False, many=False, commit=False):
         if commit:
             conn.commit()
         return result
+    v
     finally:
         cursor.close()
         conn.close()
