eb72dc962a7a51e223bb5d85032aefc6a3142061
vivekpunde05 <vivekpunde6@gmail.com>
2026-06-29 19:27:15 +0530

    new

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 # Initialize database tables
 python -c "from database import create_tables, create_default_admin; create_tables(); create_default_admin()"
