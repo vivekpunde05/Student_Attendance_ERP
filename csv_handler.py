051800a04fa95b6cf6b338c0acc3863e5372ad17
vivekpunde05 <vivekpunde6@gmail.com>
2026-07-08 21:30:44 +0530

    new

--- csv_handler.py
+++ csv_handler.py
@@ -5,6 +5,7 @@ from teacher import view_attendance
 def save_attendance_for_month(teacher_id: int, year: int, month: int, out_path: str):
     from teacher import export_by_month
     export_by_month(teacher_id, year, month, out_path)
+    
 def save_attendance_for_year(teacher_id: int, year: int, out_path: str):
     from teacher import export_by_year
     export_by_year(teacher_id, year, out_path)
